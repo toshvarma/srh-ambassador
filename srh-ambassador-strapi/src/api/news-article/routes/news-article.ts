fdece10924f6d2921a7e0460eead9a5ce4941c46
toshvarma <toshmvarma@gmail.com>
2026-06-25 11:21:59 +0200

    added policies

--- srh-ambassador-strapi/src/api/news-article/routes/news-article.ts
+++ srh-ambassador-strapi/src/api/news-article/routes/news-article.ts
@@ -1,7 +1,12 @@
-/**
- * news-article router
- */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::news-article.news-article');
+export default factories.createCoreRouter('api::news-article.news-article', {
+    config: {
+        delete: {
+            policies: ['global::is-owner'],
+        },
+        update: {
+            policies: ['global::is-owner'],
+        },
+    },
+});
